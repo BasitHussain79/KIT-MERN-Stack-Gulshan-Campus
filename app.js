5ea1c02761561b6808c9e5d6093a4d5bb567a882
BasitHussain79 <basithussainqadri@gmail.com>
2023-08-20 15:31:01 +0500

    functions complete

--- app.js
+++ app.js
@@ -1,70 +1,59 @@
-// let i = 2;
-// for (; i >= 0; ) {
-//   console.log(i);
-//   i--;
-// }
+// Regular Function
+// function initialize
 
-// let i = 0;
-// while (i <= 2) {
-//   console.log(i);
-//   i++;
-// }
-
-// let i = 4;
-
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= 1);
-
-// const a = 20;
-
-// if (a) {
-//   console.log("statement");
-// } else if (!a) {
-//   console.log("else if");
-// }
-
-// let x = "B";
-
-// switch (x) {
-//   case "A":
-//     console.log("A");
-//     break;
-//   case "B":
-//     console.log("B");
-//   // break;
-//   case "C":
-//     console.log("C");
-//   // break;
-//   default:
-//     console.log("Invalid case");
-// }
-
-// let arr = [0, 1, 2];
-
-// for (let x of arr) {
-//   console.log(x);
-// }
-
-const obj = {
-  firstName: "Basit",
-  lastName: "Hussain",
-  courses: ["MERN", "AI", "Xyz"],
-  address: null,
-};
-
-obj.email = "basit@email.com";
-obj.courses[2] = "Blockchaing";
-obj["address"] = "Xyz";
-
-// console.log(`${obj["firstName"]} ${obj["lastName"]}`, obj["courses"][2]);
-
-let keys = Object.keys(obj);
-let values = Object.values(obj);
-let objectArr = Object.entries(obj);
-console.log(keys, values, objectArr);
-
-for (let y in obj) {
-  console.log(`${y}: ${obj[y]}`);
+function name2() {
+  return "Hello";
 }
+
+function name1() {
+  return name2;
+}
+
+function funcString() {
+  return name1;
+}
+
+// function invoke/ call
+// const functionCall = funcString()()();
+// console.log(functionCall);
+
+// Function Expresion
+// const func = function () {
+//   return 2 + 2;
+// };
+
+// let a = func();
+// console.log(a);
+
+// Arrow Function
+// const arrowFunc = () => funcString;
+
+// let arrowFuncVal = arrowFunc()()()();
+// console.log(arrowFuncVal);
+
+// function regularSum(a, b) {
+//   return a + b;
+// }
+
+// const expSum = function (a, b) {
+//   return a + b;
+// };
+// const arrowSum = (a, b) => a + b;
+
+// const value = arrowSum(2, 2);
+
+function calculateProductTax(produts, tax) {
+  let total = 0;
+  let totalProductSum = 0;
+  for (let i = 0; i < produts.length; i++) {
+    total += produts[i] + produts[i] * tax;
+    totalProductSum += produts[i];
+  }
+  return {
+    productPriceWithTax: total,
+    productPriceWithoutTax: totalProductSum,
+  };
+}
+
+const calTax = calculateProductTax([20, 30, 10, 20], 0.2);
+console.log(calTax);

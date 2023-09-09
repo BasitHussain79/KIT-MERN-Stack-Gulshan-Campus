48c8bc669f234819b07b83e769125cdc0042a9b5
BasitHussain79 <basithussainqadri@gmail.com>
2023-09-09 17:05:27 +0500

    feat: add delete functionality to remove the items

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const todoForm = document.querySelector("#todoForm");
 const list = document.querySelector("ul");
 
-const restTodoList = JSON.parse(localStorage.getItem("todoList"));
+const restTodoList = localStorage.getItem("todoList")
+  ? JSON.parse(localStorage.getItem("todoList"))
+  : [];
 
 const arr = [...restTodoList];
 
@@ -16,6 +18,8 @@ function submitHandler(e) {
   const li = document.createElement("li");
   li.textContent = e.target["addTodo"].value;
   list.append(li);
+
+  document.querySelector(".emptyList").style.display = "none";
 }
 
 todoForm.addEventListener("submit", submitHandler);
@@ -23,13 +27,32 @@ todoForm.addEventListener("submit", submitHandler);
 if (localStorage?.todoList && localStorage?.todoList?.length > 0) {
   const todos = JSON.parse(localStorage.todoList);
 
-  todos.forEach((data) => {
+  todos.forEach((data, idx) => {
     const li = document.createElement("li");
-    li.textContent = data;
+    const para = document.createElement("p");
+    const btn = document.createElement("button");
+
+    li.setAttribute("data-id", idx);
+
+    para.textContent = data;
+    li.append(para);
+
+    btn.textContent = "Delete";
+    li.append(btn);
+
+    btn.setAttribute("type", "button");
+    btn.addEventListener("click", (e) => {
+      console.log(li.getAttribute("data-id"));
+      const id = li.getAttribute("data-id");
+      arr.splice(+id, 1);
+      setTodoValueInLocalStorage(arr);
+    });
+
     list.append(li);
   });
 } else {
   const li = document.createElement("li");
+  li.setAttribute("class", "emptyList");
   li.textContent = "List is empty";
   list.append(li);
 }

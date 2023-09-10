c4c1f1a3b0159e4b0168a55c629939700063d6de
BasitHussain79 <basithussainqadri@gmail.com>
2023-09-10 16:06:42 +0500

    feat: complete todo app

--- app.js
+++ app.js
@@ -13,11 +13,39 @@ const setTodoValueInLocalStorage = (data) => {
 
 function submitHandler(e) {
   e.preventDefault();
-  arr.push(e.target["addTodo"].value);
+  const randomValue = Math.floor(Math.random() * 1000);
+  arr.push({
+    todo: e.target["addTodo"].value,
+    id: randomValue,
+  });
   setTodoValueInLocalStorage(arr);
-  const li = document.createElement("li");
-  li.textContent = e.target["addTodo"].value;
-  list.append(li);
+  todoListInteractWithHTML(e.target["addTodo"].value, randomValue);
+  // const li = document.createElement("li");
+  // const para = document.createElement("p");
+  // const btn = document.createElement("button");
+  // const editBtn = document.createElement("button");
+
+  // li.setAttribute("data-id", arr.length);
+
+  // para.textContent = e.target["addTodo"].value;
+  // li.append(para);
+
+  // editBtn.textContent = "Edit";
+  // li.append(editBtn);
+
+  // btn.textContent = "Delete";
+  // li.append(btn);
+
+  // btn.setAttribute("type", "button");
+  // btn.addEventListener("click", (e) => {
+  //   console.log(li.getAttribute("data-id"));
+  //   const id = li.getAttribute("data-id");
+  //   arr.splice(+id, 1);
+  //   setTodoValueInLocalStorage(arr);
+  //   e.target.parentElement.remove();
+  // });
+
+  // list.append(li);
 
   document.querySelector(".emptyList").style.display = "none";
 }
@@ -27,33 +55,85 @@ todoForm.addEventListener("submit", submitHandler);
 if (localStorage?.todoList && localStorage?.todoList?.length > 0) {
   const todos = JSON.parse(localStorage.todoList);
 
-  todos.forEach((data, idx) => {
-    const li = document.createElement("li");
-    const para = document.createElement("p");
-    const btn = document.createElement("button");
-
-    li.setAttribute("data-id", idx);
-
-    para.textContent = data;
-    li.append(para);
-
-    btn.textContent = "Delete";
-    li.append(btn);
-
-    btn.setAttribute("type", "button");
-    btn.addEventListener("click", (e) => {
-      console.log(li.getAttribute("data-id"));
-      const id = li.getAttribute("data-id");
-      arr.splice(+id, 1);
-      setTodoValueInLocalStorage(arr);
-    });
-
-    list.append(li);
+  todos.forEach((data) => {
+    todoListInteractWithHTML(data);
   });
 } else {
   const li = document.createElement("li");
   li.setAttribute("class", "emptyList");
   li.textContent = "List is empty";
+  list.append(li);
+}
+
+function todoListInteractWithHTML(data, randomValue) {
+  const li = document.createElement("li");
+  const para = document.createElement("p");
+  const btn = document.createElement("button");
+  const editBtn = document.createElement("button");
+
+  li.setAttribute("data-id", data?.id ?? randomValue);
+
+  const editForm = document.createElement("form");
+  const editInput = document.createElement("input");
+  const updateTodo = document.createElement("button");
+
+  editInput.setAttribute("type", "text");
+  editInput.setAttribute("name", "editTodo");
+  editForm.style.display = "none";
+
+  updateTodo.setAttribute("type", "submit");
+  updateTodo.textContent = "Update";
+  editForm.append(editInput);
+  editForm.append(updateTodo);
+
+  li.append(editForm);
+
+  para.textContent = data?.todo ?? data;
+  li.append(para);
+
+  editBtn.textContent = "Edit";
+  li.append(editBtn);
+
+  btn.textContent = "Delete";
+  li.append(btn);
+
+  btn.setAttribute("type", "button");
+
+  // Edit
+  editBtn.addEventListener("click", (e) => {
+    para.style.display = "none";
+    btn.style.display = "none";
+    editBtn.style.display = "none";
+    editForm.style.display = "block";
+    editInput.value = para.textContent;
+  });
+
+  // submit edit todo
+  editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const id = li.getAttribute("data-id");
+    const updateArr = arr.map((data) => {
+      if (+data.id === +id) return { todo: editInput.value, id };
+      return data;
+    });
+    setTodoValueInLocalStorage(updateArr);
+    para.textContent = editInput.value;
+    editForm.style.display = "none";
+    para.style.display = "block";
+    btn.style.display = "block";
+    editBtn.style.display = "block";
+  });
+
+  // Delete
+  btn.addEventListener("click", (e) => {
+    console.log(li.getAttribute("data-id"));
+    const id = li.getAttribute("data-id");
+    // arr.splice(+id, 1);
+    const deleteArr = arr.filter((data) => +data.id !== +id);
+    setTodoValueInLocalStorage(deleteArr);
+    e.target.parentElement.remove();
+  });
+
   list.append(li);
 }
 
